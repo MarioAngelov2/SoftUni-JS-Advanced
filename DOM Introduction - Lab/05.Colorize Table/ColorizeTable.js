2dba02ca0b0972d27715d5fb7f5eaf5730163e24
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-13 13:32:32 +0300

    Added Exercise

--- DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,13 @@
 function colorize() {
-    // TODO
+    function colorize() {
+        // for method
+        // const rows = document.querySelectorAll('tr');
+    
+        // for (let i = 1; i < rows.length; i += 2) {
+        //     rows[i].style.backgroundColor = 'teal'
+        // }
+    
+        [...document.querySelectorAll('tr:nth-child(even)')]
+        .forEach(el => el.style.backgroundColor = 'teal')
+    }
 }
--- DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,18 @@
 function sumTable() {
-
+    function sumTable() {
+        // select all data rows (exlusive first and last row)
+        const rows = Array.from(document.querySelectorAll('tr')).slice(1, -1);
+        let sum = 0;
+    
+        // for each row
+        // - select last column
+        // - add content to sum 
+        for (let row of rows) {
+            const value = Number(row.lastElementChild.textContent);
+            sum += value;
+        }
+    
+        // - display sum in total row
+        document.getElementById('sum').textContent = sum;
+    }
 }
