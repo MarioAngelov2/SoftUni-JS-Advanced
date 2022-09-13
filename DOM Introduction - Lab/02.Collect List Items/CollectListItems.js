1220dfba53ddd90d61dafe69e229ce3081bd6f81
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-13 13:30:58 +0300

    Added Exercise

--- DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    function extractText() {
+        const items = Array.from(document.querySelectorAll('li'));
+        const result = items.map(e => e.textContent).join('\n');
+        document.getElementById('result').value = result;
+    }
 }
