8e9ab2b0a60ea23eafd6bebeb66abec025ff490b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-17 20:38:34 +0300

    Added New Exercise

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let searchBar = document.getElementById('searchField');
+   let tableRows = document.querySelectorAll('tbody tr');
 
    function onClick() {
-      //   TODO:
 
+      for (let row of tableRows) {
+         if (searchBar.value !== '' && row.innerHTML.includes(searchBar.value)) {
+            row.className = 'select'
+         } else {
+            row.classList.remove('select');
+         }
+      }
+      searchBar.value = '';
    }
 }
--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+
+  let inputText = document.getElementById('input');
+  let splittetText = inputText.value.split('.').filter(s => s !== '');
+  let outputDiv = document.getElementById('output');
+
+  while (splittetText.length > 0) {
+    let text = splittetText.splice(0, 3).join('.') + '.';
+    let newP = document.createElement('p');
+    newP.textContent = text;
+    outputDiv.appendChild(newP);
+  }
+  
 }
