baec123c5e37cdc32a9c8eb926611c59d126f421
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-17 12:37:46 +0300

    Added New Exercise

--- DOM Introduction - Exercise/04. Search in List/search.js
+++ DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let userInput = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let counter = 0;
+
+   
+   for (let town of towns) {
+      if (town.textContent.includes(userInput)) {
+         counter++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   result.textContent = `${counter} matches found`
 }
