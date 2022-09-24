3c35c2212a49ab15a1c9eaace871c297dc77b49f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-24 22:11:22 +0300

    Added solution

--- DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   let parrentElement = document.getElementById('content');
+   let elements = words;
+
+   for (let element of elements) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      let text = document.createTextNode(element);
+
+      p.appendChild(text);
+      p.style.display = 'none';
+      div.appendChild(p);
+
+      div.addEventListener('click', function (ev) {
+         ev.currentTarget.children[0].style.display = 'block';
+      });
+      parrentElement.appendChild(div);
+   }
 }
