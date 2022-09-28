3396376dc3aab97dfae1e724b4373a555abc1a3e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-09-28 23:49:01 +0300

    Update app.js

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -3,18 +3,31 @@ function encodeAndDecodeMessages() {
     button[0].addEventListener('click', encode);
     button[1].addEventListener('click', decode);
 
+    let input = document.querySelectorAll('textarea')[0]
+    let output = document.querySelectorAll('textarea')[1]
+
     function encode() {
-        let input = document.querySelectorAll('textarea')[0].value;
         let encode = '';
+        let inputText = input.value;
 
-        for (let i = 0; i < input.length; i++) {
-            let ch = input[i];
-
-            
+        for (let i = 0; i < inputText.length; i++) {
+            let ch = inputText[i];
+            encode += String.fromCharCode(ch.charCodeAt() + 1);
         }
+        output.value = encode;
+        input.value = '';
     }
 
     function decode() {
 
+        let decode = '';
+        let inputFromEncode = input.value;
+
+        for (let i = 0; i < inputFromEncode.length; i++) {
+            let currentCh = inputFromEncode[i];
+            decode += String.fromCharCode(currentCh.charCodeAt() - 1);
+        }
+
+        output.value = decode;
     }
 }
