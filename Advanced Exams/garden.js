77f915f12116a9ae43b2f2a77062657276a64a8f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-10-11 23:51:27 +0300

    Update garden.js

--- Advanced Exams/garden.js
+++ Advanced Exams/garden.js
@@ -11,15 +11,13 @@ class Garden {
         if (this.spaceAvailable < spaceRequired) {
             throw new Error("Not enough space in the garden.")
         } else if (!this.plants.includes(plantName)) {
-            this.plants.push(
-                {
-                    plantName: plantName,
-                    spaceRequired: spaceRequired,
-                    ripe: false,
-                    quantity: 0
-                }
-            )
-
+            let plantObj = {
+                plantName: plantName,
+                spaceRequired: spaceRequired,
+                ripe: false,
+                quantity: 0
+            }
+            this.plants.push(plantObj)
             this.spaceAvailable -= spaceRequired;
             return `The ${plantName} has been successfully planted in the garden.`;
         };
@@ -28,6 +26,28 @@ class Garden {
     ripenPlant(plantName, quantity) {
         quantity = Number(quantity);
 
+        let plant = this.plants.find((plant) => plant.plantName === plantName);
+
+        if (!plant) {
+            throw new Error(`There is no ${plantName} in the garden.`);
+        }
+
+        if (plant.ripe) {
+            throw new Error(`The ${plantName} is already ripe.`);
+        }
+
+        if (quantity <= 0) {
+            throw new Error(`The quantity cannot be zero or negative.`)
+        }
+
+        plant.ripe = true;
+        plant.quantity += quantity;
+
+        if (quantity === 1) {
+            return `${quantity} ${plantName} has successfully ripened.`
+        } else if (quantity > 1) {
+            return `${quantity} ${plantName}s have successfully ripened.`
+        }
     }
 
     harvestPlant(plantName) {
