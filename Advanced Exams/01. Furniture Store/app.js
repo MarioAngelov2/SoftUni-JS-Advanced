4651f465164706aefd7643ffb77122431de5339e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-10-17 18:44:39 +0300

    Update app.js

--- Advanced Exams/01. Furniture Store/app.js
+++ Advanced Exams/01. Furniture Store/app.js
@@ -1,5 +1,65 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TO DO
+    let modelInputElement = document.getElementById('model');
+    let yearInputElement = document.getElementById('year');
+    let descriptionInputElement = document.getElementById('description');
+    let priceInputElement = document.getElementById('price');
+
+    document.getElementById('add').addEventListener('click', add);
+    let furnitureList = document.getElementById('furniture-list');
+
+    function add(ev) {
+        ev.preventDefault();
+
+        if (modelInputElement === '' || yearInputElement < 0 || descriptionInputElement === '' || priceInputElement < 0) {
+            return;
+        }
+
+        let trInfo = document.createElement('tr');
+        trInfo.classList.add('info');
+        furnitureList.appendChild(trInfo);
+
+        let tdModel = document.createElement('td');
+        tdModel.textContent = `${modelInputElement.value}`;
+        trInfo.appendChild(tdModel);
+
+        let tdPrice = document.createElement('td');
+        tdPrice.textContent = `${priceInputElement.value}`;
+        trInfo.appendChild(tdPrice);
+
+        let tdButtons = document.createElement('td');
+        trInfo.appendChild(tdButtons);
+
+        let moreBtn = document.createElement('button');
+        moreBtn.classList.add('moreBtn');
+        moreBtn.textContent = 'More Info';
+        tdButtons.appendChild(moreBtn);
+        moreBtn.addEventListener('click', more);
+
+        let buyBtn = document.createElement('button');
+        buyBtn.classList.add('buyBtn');
+        buyBtn.textContent = 'Buy it';
+        tdButtons.appendChild(buyBtn);
+        //buyBtn.addEventListener('click', buy);
+
+        let trMoreInfo = document.createElement('tr');
+        trMoreInfo.classList.add('hide');
+        furnitureList.appendChild(trMoreInfo);
+
+        let tdYear = document.createElement('td');
+        tdYear.textContent = `${yearInputElement.value}`;
+        trMoreInfo.appendChild(tdYear);
+
+        let tdDescription = document.createElement('td');
+        tdDescription.setAttribute('colspan', 3);
+        tdDescription.textContent = `Description: ${descriptionInputElement.value}`;
+        trMoreInfo.appendChild(tdDescription);
+
+        function more() {
+            moreBtn.textContent = 'Less Info';
+            let trInfo = trMoreInfo.querySelector('.hide');
+            trInfo.setAttribute('style', 'display');
+        }
+    }
 }
