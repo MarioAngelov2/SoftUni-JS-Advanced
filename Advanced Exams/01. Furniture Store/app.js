c55439da5e82aae48f55b54aef0fc3aff1d66c04
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-10-17 22:04:34 +0300

    update app

--- Advanced Exams/01. Furniture Store/app.js
+++ Advanced Exams/01. Furniture Store/app.js
@@ -8,6 +8,8 @@ function solve() {
 
     document.getElementById('add').addEventListener('click', add);
     let furnitureList = document.getElementById('furniture-list');
+    let totalPrice = document.querySelector('.total-price');
+    let sum = 0;
 
     function add(ev) {
         ev.preventDefault();
@@ -25,7 +27,7 @@ function solve() {
         trInfo.appendChild(tdModel);
 
         let tdPrice = document.createElement('td');
-        tdPrice.textContent = `${priceInputElement.value}`;
+        tdPrice.textContent = `${Number(priceInputElement.value).toFixed(2)}`;
         trInfo.appendChild(tdPrice);
 
         let tdButtons = document.createElement('td');
@@ -41,14 +43,14 @@ function solve() {
         buyBtn.classList.add('buyBtn');
         buyBtn.textContent = 'Buy it';
         tdButtons.appendChild(buyBtn);
-        //buyBtn.addEventListener('click', buy);
+        buyBtn.addEventListener('click', buy);
 
         let trMoreInfo = document.createElement('tr');
         trMoreInfo.classList.add('hide');
         furnitureList.appendChild(trMoreInfo);
 
         let tdYear = document.createElement('td');
-        tdYear.textContent = `${yearInputElement.value}`;
+        tdYear.textContent = `Year: ${yearInputElement.value}`;
         trMoreInfo.appendChild(tdYear);
 
         let tdDescription = document.createElement('td');
@@ -56,10 +58,32 @@ function solve() {
         tdDescription.textContent = `Description: ${descriptionInputElement.value}`;
         trMoreInfo.appendChild(tdDescription);
 
-        function more() {
-            moreBtn.textContent = 'Less Info';
-            let trInfo = trMoreInfo.querySelector('.hide');
-            trInfo.setAttribute('style', 'display');
+        clearInputs(modelInputElement, yearInputElement, descriptionInputElement, priceInputElement)
+
+        function more(ev) {
+            if (ev.target.textContent === 'More Info') {
+                ev.target.textContent = 'Less Info';
+                trMoreInfo.style.display = 'contents';
+            } else {
+                ev.target.textContent = 'More Info';
+                trMoreInfo.style.display = 'none';
+            }
+        }
+
+        function buy(ev) {
+            let furnitureInfo = ev.target.parentElement.parentElement;
+            furnitureInfo.remove();
+
+            sum += Number(tdPrice.textContent)
+
+            totalPrice.textContent = `${sum.toFixed(2)}`
+        }
+
+        function clearInputs(modelInputElement, yearInputElement, descriptionInputElement, priceInputElement) {
+            modelInputElement.value = '';
+            yearInputElement.value = '';
+            descriptionInputElement.value = '';
+            priceInputElement.value = '';
         }
     }
 }
