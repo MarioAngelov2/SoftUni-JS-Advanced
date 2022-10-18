7e4941f7a62c0cd01bad43f5027d29bdba5296fe
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-10-19 00:20:36 +0300

    Update solution.js

--- Advanced Exams/01. Travel Agency/solution.js
+++ Advanced Exams/01. Travel Agency/solution.js
@@ -1,5 +1,37 @@
 window.addEventListener('load', solution);
 
 function solution() {
-  console.log('TODO: Write the missing functionality!');
+  let fullNameInputElement = document.getElementById('fname');
+  let emailInputElement = document.getElementById('email');
+  let phNumberInputElement = document.getElementById('phone');
+  let addresInputElement = document.getElementById('address');
+  let postCodeInputElement = document.getElementById('code');
+
+  document.getElementById('submitBTN').addEventListener('click', submit);
+
+  let infoPreviewListElement = document.getElementById('infoPreview');
+
+  function submit(ev) {
+    ev.preventDefault();
+
+    let fName = document.createElement('li');
+    fName.textContent = `Full Name: ${fullNameInputElement.value}`;
+    infoPreviewListElement.appendChild(fName);
+
+    let email = document.createElement('li');
+    email.textContent = `Email: ${emailInputElement.value}`
+    infoPreviewListElement.appendChild(email);
+
+    let phNumber = document.createElement('li');
+    phNumber.textContent = `Phone Number: ${phNumberInputElement.value}`;
+    infoPreviewListElement.appendChild(phNumber);
+
+    let address = document.createElement('li');
+    address.textContent = `Address: ${addresInputElement.value}`;
+    infoPreviewListElement.appendChild(address);
+
+    let postCode = document.createElement('li');
+    postCode.textContent = `Postal Code: ${postCodeInputElement.value}`
+    infoPreviewListElement.appendChild(postCode);
+  }
 }
